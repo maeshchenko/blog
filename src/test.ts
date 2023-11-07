66d9c29a496f37adc30519c89580448703b8eabe
Mikhail Eshchenko <eschenko.mihail@wb.ru>
2023-11-07 12:01:04 +0700

    add webpack devserver and env

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,7 @@
 export function testFunction(arg:string):string {
     console.log('TEST FUNCTION: ', arg);
 
+    document.getElementsByTagName('body')[0].style.background = 'red'
+
     return arg;
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,24 @@
 import path from "path";
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {IBuildPaths} from "./config/build/types/config";
+import {IBuildEnv, IBuildPaths} from "./config/build/types/config";
 
 const paths: IBuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
     build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html')
 }
-const mode = 'development';
-const isDev = mode === 'development'
 
-const config = buildWebpackConfig({
-    mode,
-    paths,
-    isDev
-});
+export default (env:IBuildEnv)=>{
+    const mode = env.mode || 'development';
+    const isDev = mode === 'development';
+    const PORT = env.port || 3000;
 
-export default config;
+    const config = buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port: PORT
+    });
+
+    return config;
+};
