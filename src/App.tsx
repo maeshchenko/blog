998f51e83b72d62d39b4542404c137bc23af151f
Mikhail Eshchenko <mikhail.eshchenko@gmail.com>
2023-11-09 21:35:23 +0700

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
-import './index.scss';
+import './styles/index.scss';
 
 export const App = () => {
     return (
